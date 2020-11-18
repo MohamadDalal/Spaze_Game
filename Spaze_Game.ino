d38df08028ac95a36b4e401ca39defed8d70bed1
GO_AWAY_77 <mohammad.moddy@gmail.com>
2020-11-18 13:36:20 +0100

    This time the code is verifyable

--- Spaze_Game.ino
+++ Spaze_Game.ino
@@ -5,7 +5,9 @@
 
 U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI   u8g2  (U8G2_R0, 5, 22, 17);
 
-int TempBitmapPos[2] = {0,1};
+//static unsigned char Bitmap[8];
+int TempBitmapPos[] = {0,0};
+int *PntTempBitmapPos = TempBitmapPos;
 
 
 struct Ship
@@ -26,23 +28,53 @@ struct Ship
   unsigned char *PntBitmap1;  //Pointer to first bitmap
   unsigned char *PntBitmap2;  //Pointer to second bitmap
   unsigned char *PntBitmap3;  //Pointer to third bitmap
-  
-}
+};
 
 unsigned char * Get_Bitmap(int Num)
 {
   switch(Num)
   {
     case 1:           //Player ship 1 (Left part)
-      
-      static unsigned char Bitmap[] = {0x04, 0x03, 0x05, 0x06, 0x05, 0x03, 0x04};
+      *PntTempBitmapPos = 7;    //Assign height of bitmap
+      PntTempBitmapPos++;       //Go up to width field
+      *PntTempBitmapPos = 3;    //Assign width of bitmap
+      PntTempBitmapPos--;       //Go back to height field
+      static unsigned char Bitmap1[] = {0x04, 0x03, 0x05, 0x06, 0x05, 0x03, 0x04};
+      return Bitmap1
+/*    Killed cause I don't think that the right method, but I am not sure
+      //Write the bitmap
+      Bitmap[0] = 0x04;
+      Bitmap[1] = 0x03;
+      Bitmap[2] = 0x05;
+      Bitmap[3] = 0x06;
+      Bitmap[4] = 0x05;
+      Bitmap[5] = 0x03;
+      Bitmap[6] = 0x04;
+      Bitmap[7] = 0x00;
+
+      return Bitmap;            //Return a pointer to the bitmap
+*/
+    case 2:           //Player ship 2 (Right part)
+      *PntTempBitmapPos = 3;    //Assign height of bitmap
+      PntTempBitmapPos++;       //Go up to width field
+      *PntTempBitmapPos = 6;    //Assign width of bitmap
+      PntTempBitmapPos--;       //Go back to height field
+      static unsigned char Bitmap2[] = {0x22, 0x1d, 0x22};
+      return Bitmap2
+/*
+      Bitmap[0] = 0x22; 
+      Bitmap[1] = 0x1d;
+      Bitmap[2] = 0x22;
+      Bitmap[3] = 0x00;
+      Bitmap[4] = 0x00;
+      Bitmap[5] = 0x00;
+      Bitmap[6] = 0x00;
+      Bitmap[7] = 0x00;
       return Bitmap;
-    case 2:
-      static unsigned char Bitmap[] = {0x22, 0x1d, 0x22};
-      return Bitmap;
+*/
     default:
       Serial.print("Bitmap out of range");
-      return
+      return Bitmap;
   }
 }
 
